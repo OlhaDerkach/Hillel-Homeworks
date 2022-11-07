13a02bb6dd5964b88ccd7f604e639358ec8d3d7b
OlhaDerkach <48595439+OlhaDerkach@users.noreply.github.com>
2022-11-07 10:06:07 +0200

    Update homework9.py

--- HW9/homework9.py
+++ HW9/homework9.py
@@ -6,19 +6,10 @@ def check_paired(num):
     else:
         return False
 
-
 num = 12
 my_primes = [2, 3, 5, 7]
 
-# написати до кожної функції мінімум 5 assert
-assert num != str(num), "Integer is needed"
-assert num not in my_primes, "This prime number is not allowed"
-assert num != 0, "0 is not a natural number!"
-assert 9 < num < 100, "Only two digit numbers required"
-assert num != False, "Only paired number is needed"
-
 print(check_paired(num))
-
 
 # напишіть функцію. яка приймає стрічку, і визначає, чи дана стрічка відповідає результатам роботи
 # методу capitalize() (перший символ є верхнім регістром, а решта – нижнім.) (повертає True False)
@@ -31,18 +22,9 @@ def check_capitalized(my_str):
     else:
         return False
 
-
 my_str = "RavO"
 
-# написати до кожної функції мінімум 5 assert
-assert my_str != "Hillel", "This value is not provided"
-assert len(my_str) <= 10, "String is TOO long"
-assert len(my_str) >= 1, "String is TOO short"
-assert my_str.isspace() != True, "We don't need only spaces!"
-assert my_str.isupper() != True, "Only first letter shall be capitalised"
-
 print(check_capitalized(my_str))
-
 
 # написати декоратор, який добавляє принт з результатом роботи отриманої функції + текстовий параметр,
 # отриманий ним (декоратор з параметром - це там, де три функції)
